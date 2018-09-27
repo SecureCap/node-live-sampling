9435d8050913d2b3033690a894fef402e365da25
Secure Cap <Twilliams397tw@gmail.com>
2018-09-27 08:43:23 -0500

    added userclip components and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,151 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './appComponents/navbar'
+import AudioContainer from './appComponents/audioContainer'
+import Login from './appComponents/login'
+import Signup from './appComponents/signup'
+import {Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 
+const LOGINURL = 'http://localhost:3000/login'
+const USERSURL = 'http://localhost:3000/users'
+
 class App extends Component {
+
+  state = {
+    logged_in: false,
+    user: {}
+  }
+
+  fetchLoginUser(username, password) {
+    return fetch(LOGINURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    .then(res => res.json())
+  }
+
+  LogIn = (event) => {
+    event.preventDefault()
+    let username = event.target.username.value
+    let password = event.target.password.value
+    this.fetchLoginUser(username, password)
+    .then(data => {
+      if (data.message === "User Not Found") {
+        this.setState({
+          logged_in: false,
+          user: data
+        })
+      } else {
+        this.setState({
+          user: data,
+          logged_in: true
+        })
+        localStorage.setItem("user_id", data.id)
+      }
+    })
+  }
+
+  fetchCreateUser(username, password, passwordConfirmation) {
+    return fetch(USERSURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirmation
+      })
+    }).then(res => res.json())
+  }
+
+  createUser = (event) => {
+    event.preventDefault()
+    let username = event.target.username.value
+    let password = event.target.password.value
+    let passwordConfirmation = event.target.passwordConfirmation.value
+    this.fetchCreateUser(username, password, passwordConfirmation)
+    .then(data => {
+      if(data.message === "Invalid Information. Please try again") {
+        this.setState({
+          logged_in: false,
+          user: data
+        })
+      } else {
+        this.setState({
+          logged_in: true,
+          user: data
+        })
+      }
+      localStorage.setItem("user_id", data.id)
+    })
+  }
+
+  LogOut = (event) => {
+    localStorage.clear()
+    this.setState({
+      logged_in: false,
+      user: {}
+    })
+  }
+
+  fetchUser(id) {
+    return fetch(`${USERSURL}/${id}`).then(res => res.json())
+  }
+
+  componentDidMount() {
+    if (localStorage["user_id"]) {
+      let id = parseInt(localStorage.user_id)
+      this.fetchUser(id)
+      .then(data => {
+        this.setState({
+          user: data
+        })
+      })
+    }
+  }
+
+  whatToRender() {
+    if (this.state.user.username) {
+      return (
+        <div className="LoggedIn">
+          <NavBar Logout={this.LogOut} user={this.state.user}/>
+          <Switch>
+            <Route path="/users/:id" render={(routerParams) => {
+              return <AudioContainer ref="audioContainer" clips={this.state.user.clips} user={this.state.user.id}/>
+            }}/>
+            <Redirect from="/" to="/users/:id" />
+          </Switch>
+        </div>
+      )
+    } else {
+      return (
+        <div className="NotLoggedIn">
+          <NavBar user={this.state.user}/>
+          <Switch>
+            <Route path="/login" render={(routerParams) => {
+              return <Login message={this.state.user.message} handleLogin={this.LogIn}/>
+            }}/>
+            <Route path="/signup" render={(routerParams) => {
+              return <Signup message={this.state.user.message} handleSignUp={this.createUser} />
+            }}/>
+            <Redirect from="/" to="/login" />
+          </Switch>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.whatToRender()}
       </div>
     );
   }
