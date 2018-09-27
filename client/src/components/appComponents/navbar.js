a0d1208c1a1a472190594f09f0fac270ad147762
Secure Cap <Twilliams397tw@gmail.com>
2018-09-27 08:33:21 -0500

    added app components and loaded clip components

---
+++ client/src/components/appComponents/navbar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const setTimeOfDay = (time) => {
+  if (time < 12) {
+    return "Morning"
+  } else if (time >= 18) {
+    return "Evening"
+  } else {
+    return "Afternoon"
+  }
+}
+
+const userExists = (props) => {
+  const time = new Date().getHours()
+  if (props.user.username) {
+    return (
+      <div id="userLogin">
+        <h3><NavLink to="/users/:id" exact>Good {setTimeOfDay(time)}, {props.user.username}</NavLink></h3>
+        <h3>|</h3>
+        <h3 onClick={props.Logout}><NavLink to='/login' exact>Logout</NavLink></h3>
+      </div>
+    )
+  } else if (window.location.pathname === "/login") {
+    return (
+      <div id="userLogin">
+        <h3><NavLink to='/signup' exact>Sign Up</NavLink></h3>
+      </div>
+    )
+  } else {
+    return (
+      <div id="userLogin">
+        <h3><NavLink to='/login' exact>Login</NavLink></h3>
+      </div>
+    )
+  }
+}
+
+const NavBar = (props) => {
+  return (
+    <div id="navBar">
+      <div id="logoTitle">
+        <img src='../orangeLogo.png' alt="Behind the Music"/>
+      </div>
+      <div>
+        {userExists(props)}
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ client/src/components/loadedClipComponents/loadedClipEdit.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const LoadedClipEdit = (props) => {
+  return (
+    <div className="AudioClip">
+      <form id="editForm" onSubmit={props.updateClip}>
+        <h2>EDIT YOUR CLIP</h2>
+        <div className="signupInput">
+        <h3>Title: </h3>
+        <input type="text" name="title" placeholder={props.clip.title} onChange={props.updateTitle}/>
+        </div>
+        <div className="signupInput">
+        <h3>Artist: </h3>
+        <input type="text" name="artist" placeholder={props.clip.artist} onChange={props.updateArtist}/>
+        </div>
+        <br/ >
+        <input id="submit" type="submit" value="Submit" />
+      </form>
+      <input id="edit" type="submit" onClick={props.endEdit} value="Go Back"/>
+      <br />
+      <input id="delete" type="submit" onClick={props.deleteClip} value="Delete Clip"/>
+    </div>
+  )
+}
+
+export default LoadedClipEdit
