6e8531844961ae4db3e982f1ca56c9b284011aaa
Abraham Ibarra <aibarra988@gmail.com>
2018-09-27 22:12:24 -0500

    Added passport-jwt authentication

--- models/User.js
+++ models/User.js
@@ -1,14 +1,33 @@
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
-const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String }
+    username: { type: String, required: true},
+    password: { type: String, required: true }
 })
 
-userSchema.plugin(passportLocalMongoose)
+userSchema.pre('save', function(next) {
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.hash(this.password, 10, (err, hash) => {
+            console.log('hi from bcrypt hash', hash)
+            if (err) {
+                return next(err)
+            } 
+            this.password = hash
+            return next()
+        })
+    }
+})
+
+userSchema.methods.comparePassword = function(pass, cb) {
+    bcrypt.compare(pass, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err)    
+        }
+        cb(null, isMatch)
+    })
+} 
 
 const User = mongoose.model('User', userSchema)
 
