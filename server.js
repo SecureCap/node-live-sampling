66c07fe8735c7b1848ed1ec9f3ad384bf96df140
Secure Cap <Twilliams397tw@gmail.com>
2018-10-04 14:04:06 -0500

    configuring s3 uplaod

--- server.js
+++ server.js
@@ -7,7 +7,89 @@ const routes = require('./routes')
 const PORT = process.env.PORT || 3001
 const User = require('./models/User')
 
+require('dotenv').config();
+const AWS = require('aws-sdk');
+const UUID = require('uuid/v4');
+const Busboy = require('busboy')
 
+//set up s3
+const {
+    SERVER_PORT,
+    AWS_REGION,
+    AWS_BUCKET,
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY
+  } = process.env;
+  
+  AWS.config.update({
+    region: AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY
+  });
+  
+  const S3 = new AWS.S3();
+
+  // GENERAL
+app.post('/api/uploadObject', (req, res) => {
+    let chunks = [],
+      fname,
+      ftype,
+      fEncoding;
+  
+    console.log(req.headers);
+  
+    let busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      console.log(
+        'File [' +
+          fieldname +
+          ']: filename: ' +
+          filename +
+          ', encoding: ' +
+          encoding +
+          ', mimetype: ' +
+          mimetype
+      );
+  
+      fname = filename.replace(/ /g, '_');
+      ftype = mimetype;
+      fEncoding = encoding;
+      file.on('data', function(data) {
+        console.log(chunks);
+        chunks.push(data);
+      });
+      file.on('end', function() {
+        console.log('File [' + filename + '] Finished');
+      });
+    });
+    busboy.on('finish', function() {
+      const userId = UUID();
+      const params = {
+        Bucket: AWS_BUCKET, // your s3 bucket name
+        Body: Buffer.concat(chunks), // concatinating all chunks
+        Key: `${userId}-${fname}`,
+        ContentType: ftype, // required
+        ACL: 'public-read'
+      };
+      // we are sending buffer data to s3.
+      S3.upload(params, (err, s3Response) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send({ err, status: 'error', msg: 'Upload Failed!' });
+        } else {
+          console.log(s3Response);
+          res.status(200).send({
+            data: s3Response,
+            status: 'success',
+            msg: 'Upload Successful!'
+          });
+        }
+      });
+    });
+    req.pipe(busboy);
+  });
+
+////////////////////////////////////////
 // Setup passport
 require('./config/passport')
 
@@ -21,15 +103,7 @@ app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static('client/build'))
 
-//You can use the Express router that is bundled with this module to answer calls to /s3/sign
-// app.use('/s3', require('react-s3-uploader/s3router')({
-//     bucket: "DevShops",
-//     region: 'us-east-1', //optional
-//     signatureVersion: 'v4', //optional (use for some amazon regions: frankfurt and others)
-//     headers: {'Access-Control-Allow-Origin': '*'}, // optional
-//     ACL: 'private', // this is default
-//     uniquePrefix: true // (4.0.2 and above) default is true, setting the attribute to false preserves the original filename in S3
-// }));
+
 
 // Setup routes
 app.use(routes);
