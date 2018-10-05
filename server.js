60e1e2d297a4a9cfebda94c0ffab02f7a11cecd0
Secure Cap <Twilliams397tw@gmail.com>
2018-10-05 10:11:55 -0500

    added engines for heroku deployment

--- server.js
+++ server.js
@@ -90,6 +90,21 @@ app.post('/api/uploadObject', (req, res) => {
   });
 
 ////////////////////////////////////////
+
+// Set up a whitelist and check against it:
+var whitelist = ['http://localhost:3001', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
 // Setup passport
 require('./config/passport')
 
